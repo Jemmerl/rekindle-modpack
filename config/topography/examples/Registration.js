b28d70fb56aaab6563f3670b728e03af7e761774
Jemmerl <jemmerl@comcast.net>
2022-05-24 23:39:45 -0400

    Removed topography temp

--- config/topography/examples/Registration.js
+++
@@ -1,9 +0,0 @@
-/*
- * This file is used to register things like biomes, items, blocks, etc. Topography searches for all scripts ending with Registration.js and executes them.
- */
-
-/*
- * Registers a biome.
- * Currently extremely simple. Basically makes a blank biome.
- */
-register(Util.Biomes.makeBiome("topography:infinite_dark"));
